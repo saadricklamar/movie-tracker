7261198d5dee6736545450ba47eac18ff8bb4653
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-07 16:08:37 -0600

    Add updated fetch functions

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import {fetchData} from '../../util/fetchData.js';
+import {key} from '../../util/key.js';
 
 
 class App extends Component {
@@ -10,17 +12,16 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-      const urlFilms = 'https://api.themoviedb.org/3/discover/movie?api_key=e694811cabd9b44f812bc17ce1606c63&/discover/movie?primary_release_year=2010&sort_by=vote_average.desc';
-      fetch(urlFilms)
-        .then(response => response.json())
-        .then(results => this.setState({movies: results.results}))
-        .catch(err => console.log(err))
-  }
+componentDidMount(){
+  this.fetchMovies()
+}
 
-
-
-
+fetchMovies= async ()=>{
+  const url= `https://api.themoviedb.org/3/discover/movie?api_key=${key}&/discover/movie?primary_release_year=2010&sort_by=vote_average.desc`;
+  fetchData(url)
+  let uncleanMovies = await fetchData(url)
+  return uncleanMovies
+}
 
   render() {
     console.log(this.state.movies)
---
+++ src/util/key.js
@@ -0,0 +1,1 @@
+export const key = 'fb457ec4329812194b77f8e9822b2001';
