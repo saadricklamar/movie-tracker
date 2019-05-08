2eecd6307bf0916dd3ecbde84f71c888d81f76e1
Saad Baradan <saadbaradan@gmail.com>
2019-05-08 15:07:31 -0600

    Make movie images display
    co-authored-by: Bridgett Coyle<bacoyle0409@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import {fetchData} from '../../util/fetchData.js';
 import {key} from '../../util/key.js';
 import {cleanMovieData} from '../../util/helpers.js';
-
+import Login from '../Login/Login';
 
 
 class App extends Component {
@@ -29,30 +29,15 @@ fetchMovies= async ()=>{
 }
 
   render() {
-    const movies = this.state
+    const {movies} = this.state
     const displayMovies = movies.length && movies.map(movie => {
-     return <img alt='Movie posters for current top movies' src={movie.posterImage} />
+     return <img src={movie.posterImage} />
    })
     return (
       <div className='App'>
-      <section className='container'>
-        <header>
-          <h1>Movie <i class="fas fa-film"></i> Tracker</h1>
-      {displayMovies}
-        </header>
-        <main>
-          <form>
-            <label for='email-input'>Email</label>
-            <input type='text' id='email-input' placeholder='Email'></input>
-            <label for="email-input">Password</label>
-            <input type= 'text' id='password-input' placeholder='Password'></input>
-            <input type='submit' value='Login' className='login'></input>
-            <h2>Don't Have An Account?</h2>
-            <a href='blank'><p>Create An Account</p></a>
-          </form>
-        </main>
-      </section>
-    </div>
+        <Login />
+        {/* {displayMovies} */}
+      </div>
     );
   }
 }
