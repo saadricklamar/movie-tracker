31f0f711bb718b3279b9e99b93b0261ac2e7fcc4
Saad Baradan <saadbaradan@gmail.com>
2019-05-11 16:53:22 -0600

    Add functionality that validates email
    co-authored-by: Bridgett Coyle<bacoyle0409@gmail.com>

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -20,26 +20,9 @@ export class MovieContainer extends Component  {
       const url= `https://api.themoviedb.org/3/discover/movie?api_key=${key}&/discover/movie?primary_release_year=2010&sort_by=vote_average.desc`;
       await this.props.loadMovies(url);
     }
-    
-    // fetchMovies= async ()=>{
-    //   const url= `https://api.themoviedb.org/3/discover/movie?api_key=${key}&/discover/movie?primary_release_year=2010&sort_by=vote_average.desc`;
-    //   fetchData(url)
-    //   let response = await fetchData(url)
-    //   let movies = cleanMovieData(response)
-    //   this.setState({
-    //     movies
-    //   })
-    // }
-    
-
 
   render() {
-    console.log(this.props['user'])
-    console.log(this.props.movies)
     const {movies} = this.props
-    // const displayMovies = movies.length && movies.map(movie => {
-    //     return <img src={movie.posterImage} />
-    // })
     return (
     <section> 
         <header>
@@ -47,13 +30,13 @@ export class MovieContainer extends Component  {
             <h1 className='container-h1'>Movie <i className="fas fa-film"></i> Tracker</h1>
           </section>
         <nav role="navigation">
-          <div id="menuToggle">
+          <div id="menuToggle"> 
             <input type="checkbox" />
                 <span></span>
                 <span></span>
-                <span></span>
+                <span></span>   
             <ul id="menu">
-              <h4 className="user-name">{this.props['user'].name}</h4>
+            <h4 className="user-name">Welcome, {this.props['user'].name}</h4>
               <a href="#"><li>Home</li></a>
               <hr></hr>
               <a href="#"><li>Favorite</li></a>
--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -6,3 +6,20 @@ export const fetchData = async(url, options) => {
         throw Error('Error receiving data from fetch')
     }
 }   
+
+export const addUser = async (user) => {
+    try  {
+        const response = await fetch('http://localhost:3000/api/users/new', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        return data
+    } catch(error) {
+        throw new Error()
+    }
+}
+   
