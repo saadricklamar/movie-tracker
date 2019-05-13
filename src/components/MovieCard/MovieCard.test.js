cc8c5c2932d037d2e4f743bfb7db290ccfdb3d9c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-05-13 07:27:06 -0600

    Add tests (failing) to MovieCard component and tests(passing) for fetchData

--- src/components/MovieCard/MovieCard.test.js
+++ src/components/MovieCard/MovieCard.test.js
@@ -1,16 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MovieCard from './MovieCard';
+import {shallow } from 'enzyme';
+import {mapStateToProps} from './MovieCard'
+import * as actions from '../../util/fetchData';
 
+
+jest.mock('../../util/fetchData')
 
 
 describe('MovieCard', () => {
-    let wrapper
+    let wrapper;
+    let mockMovie;
+    
+    beforeEach(() => {
+      mockMovie={
+        title: 'Avengers',
+        rating: 10,
+        id: 1,
+        releaseDate: 2019,
+        synopsis: 'a darn good movie',
+        posterImage:  undefined 
+
+      }
+      wrapper = shallow(
+        <MovieCard movie={mockMovie} user_id={1}/>
+      );
   
-  it('should match the snapshot with all data passed in', () => {
-    expect(wrapper).toMatchSnapshot();
-    });
-  
-  
-  
-  })
+    })
+
+    describe('MovieCard Component', () => {
+
+      it('should match the snapshot with all data passed in correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      describe('ComponentDidMount', () => {
+        it('should call getFavorites on componentDidMount', async () => {
+          actions.getFavorites.mockImplementation(() => ({data: [mockMovie]}))
+          wrapper = shallow(<MovieCard movie={mockMovie} user_id={1}/>, { disableLifecycleMethods: true })
+          await wrapper.instance().componentDidMount()
+          expect(actions.getFavorites).toHaveBeenCalled()
+        });
+
+
+        it('set favorite to true if its a current favorite', async () => {
+          actions.getFavorites.mockImplementation(() => ({data: [{movie_id: 1}]}))
+          wrapper = shallow(<MovieCard movie={mockMovie} user_id={1}/>, { disableLifecycleMethods: true })
+          await wrapper.instance().componentDidMount()
+          expect(wrapper.state().favorite).toBe(true)
+        });
+
+        it('set favorite to false if its a current favorite', async () => {
+          actions.getFavorites.mockImplementation(() => ({data: [{movie_id: 2}]}))
+          wrapper = shallow(<MovieCard movie={mockMovie} user_id={1}/>, { disableLifecycleMethods: true })
+          await wrapper.instance().componentDidMount()
+          expect(wrapper.state().favorite).toBe(false)
+        })
+
+      })
+    })
+})
